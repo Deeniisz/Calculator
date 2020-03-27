a0a5362b75230a0d04e8e78c8aa1abb96e5af7e6
Deeniisz <42427057+Deeniisz@users.noreply.github.com>
2020-03-27 15:26:56 -0300

    Update Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -1,18 +1,6 @@
-import java.rmi.Naming;
-import java.rmi.Remote;
-import java.rmi.registry.LocateRegistry;
-
-public class CalculatorServer implements Remote{
-    public CalculatorServer(){
-        try{
-            Calculator c = new CalculatorImpl();
-            LocateRegistry.createRegistry(1020);
-            Naming.rebind("//localhost:1020/CalculatorService", c);
-        } catch (Exception e){
-            System.out.println("Trouble: " + e);
-        }
-    }
-    public static void main(String args[]){
-        new CalculatorServer();
-    }
+public interface Calculator extends java.rmi.Remote {
+    public long add(long a, long b) throws java.rmi.RemoteException;
+    public long sub(long a, long b) throws java.rmi.RemoteException;
+    public long mul(long a, long b) throws java.rmi.RemoteException;
+    public long div(long a, long b) throws java.rmi.RemoteException;
 }
